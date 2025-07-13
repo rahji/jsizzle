958730eb09333a87be0f366782990ca14deac5db
Rob Duarte <me@robduarte.com>
2025-07-13 07:10:42 -0400

    added bubbletea textarea but didn't connect js vm yet

--- main.go
+++ main.go
@@ -3,34 +3,86 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 
+	"github.com/charmbracelet/bubbles/textarea"
+
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/dop251/goja"
 )
 
+type errMsg error
+
+type model struct {
+	textarea textarea.Model
+	err      error
+}
+
+func initialModel() model {
+	ti := textarea.New()
+	ti.Focus()
+	return model{textarea: ti, err: nil}
+}
+
+func (m model) Init() tea.Cmd {
+	return textarea.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyEsc:
+			if m.textarea.Focused() {
+				m.textarea.Blur()
+			}
+		case tea.KeyCtrlC:
+			return m, tea.Quit
+		case tea.KeyCtrlG:
+			return m, tea.Quit
+		default:
+			if !m.textarea.Focused() {
+				cmd = m.textarea.Focus()
+				cmds = append(cmds, cmd)
+			}
+		}
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	m.textarea, cmd = m.textarea.Update(msg)
+	cmds = append(cmds, cmd)
+	return m, tea.Batch(cmds...)
+}
+
+func (m model) View() string {
+	return fmt.Sprintf("\n%s\n\n%s",
+		m.textarea.View(),
+		"(ctrl+g to run, ctrl+c to quit)",
+	) + "\n\n"
+}
+
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: go run main.go <javascript-file>")
-		os.Exit(1)
-	}
-
-	filename := os.Args[1]
-
-	jsCode, err := os.ReadFile(filename)
-	if err != nil {
-		log.Fatalf("Error reading file %s: %v", filename, err)
-	}
-
 	vm := goja.New()
-
 	console := vm.NewObject()
 	console.Set("log", func(args ...interface{}) {
 		fmt.Println(args...)
 	})
 	vm.Set("console", console)
 
-	_, err = vm.RunString(string(jsCode))
-	if err != nil {
-		log.Fatalf("Error executing JavaScript: %v", err)
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
 	}
+}
+
+func runJavascript(vm *goja.Runtime, src string) error {
+	_, err := vm.RunString(src)
+	if err != nil {
+		return err
+	}
+	return nil
 }
