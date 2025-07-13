647cb5893c2280994f826cb066546c0fb86aab5e
Rob Duarte <me@robduarte.com>
2025-07-13 08:46:34 -0400

    errors display but console.log doesn't

--- main.go
+++ main.go
@@ -12,9 +12,16 @@ import (
 
 type errMsg error
 
+type jsReturnMsg struct {
+	err string
+}
+
 type model struct {
-	textarea textarea.Model
-	err      error
+	textarea  textarea.Model
+	jsOutput  string
+	status    string
+	runningJs bool
+	err       error
 }
 
 func initialModel() model {
@@ -25,6 +32,22 @@ func initialModel() model {
 
 func (m model) Init() tea.Cmd {
 	return textarea.Blink
+}
+
+func (m *model) runJS() tea.Cmd {
+	return func() tea.Msg {
+		vm := goja.New()
+		console := vm.NewObject()
+		console.Set("log", func(args ...interface{}) {
+			m.jsOutput = fmt.Sprintln(args...)
+		})
+		vm.Set("console", console)
+		_, err := vm.RunString(m.textarea.Value())
+		if err != nil {
+			return jsReturnMsg{err: err.Error()}
+		}
+		return jsReturnMsg{err: ""}
+	}
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -41,12 +64,24 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case tea.KeyCtrlC:
 			return m, tea.Quit
 		case tea.KeyCtrlG:
-			return m, tea.Quit
+			cmd = m.runJS()
+			// fmt.Println(m.jsOutput) //xxx
+			cmds = append(cmds, cmd)
+			m.runningJs = true
 		default:
 			if !m.textarea.Focused() {
 				cmd = m.textarea.Focus()
 				cmds = append(cmds, cmd)
 			}
+		}
+	case jsReturnMsg:
+		m.runningJs = false
+		if msg.err != "" {
+			// xxx this should show up in a status line or output window in red?
+			m.status = msg.err
+		} else {
+			// xxx this should show up in output window
+			m.status = m.jsOutput
 		}
 	case errMsg:
 		m.err = msg
@@ -59,30 +94,16 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	return fmt.Sprintf("\n%s\n\n%s",
+	return fmt.Sprintf("\n%s\n\n%s %s",
 		m.textarea.View(),
 		"(ctrl+g to run, ctrl+c to quit)",
+		m.status,
 	) + "\n\n"
 }
 
 func main() {
-	vm := goja.New()
-	console := vm.NewObject()
-	console.Set("log", func(args ...interface{}) {
-		fmt.Println(args...)
-	})
-	vm.Set("console", console)
-
 	p := tea.NewProgram(initialModel())
 	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func runJavascript(vm *goja.Runtime, src string) error {
-	_, err := vm.RunString(src)
-	if err != nil {
-		return err
-	}
-	return nil
 }
